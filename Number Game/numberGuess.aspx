03517605aadd773eea154f3997dd7d487655e7a6
laypatel2910 <lay.manavadariya@gmail.com>
2024-10-10 03:03:28 +0530

    Add project files.

---
+++ Number Game/numberGuess.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="numberGuess.aspx.cs" Inherits="Number_Game.numberGuess" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Number Guess</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"/>
+</head> 
+<body>
+    <form id="form1" runat="server" class="position-absolute top-50 start-50 translate-middle">
+        <div>
+            <center><h1 class="h3 mb-3 font-weight-normal">Welcome to Number Game</h1></center>
+            <asp:Label ID="lblComputerGuess" runat="server" Text="I'm thinking of a number between 1 and 100 : "></asp:Label><br />
+            <div class="form-floating">
+                <asp:Label ID="lblUserGuess" runat="server" Text="Tell Me What Number I Guess"></asp:Label><br />
+                <asp:Label ID="lblattempts" runat="server" Text="You have 10 attempts to guess it." Font-Bold="true"></asp:Label><br />
+                <asp:TextBox class="form-control" ID="txtNumberGuess" runat="server" placeholder="Enter a number : "></asp:TextBox>
+                <asp:RegularExpressionValidator ID="RegularExpressionValidatorNumber" runat="server" ErrorMessage="Enter number only" ForeColor="Red" ControlToValidate="txtNumberGuess" ValidationExpression="[0-9]+" Display="Dynamic" Font-Size="Small" ></asp:RegularExpressionValidator>
+                <asp:RequiredFieldValidator ID="RequiredNumber" runat="server" ErrorMessage="please Enter number" ForeColor="Red" ControlToValidate="txtNumberGuess" Display="Dynamic"></asp:RequiredFieldValidator>
+                <asp:Label ID="lblMessage" runat="server" ForeColor="Red" Text=""></asp:Label>
+            </div>
+            <asp:Button ID="btnSubmitValue" class="btn btn-primary w-100 py-2" runat="server" Text="Entered Number" OnClick="btnSubmitValue_Click"/>
+        </div>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ Number Game/numberGuess.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Number_Game
+{
+    public partial class numberGuess : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Random rnd = new Random();
+                Session["RandomNumber"] = rnd.Next(1, 101);
+                Session["AttemptCount"] = 10;
+            }
+        }
+
+        protected void btnSubmitValue_Click(object sender, EventArgs e)
+        {
+            if (Session["RandomNumber"] == null || Session["AttemptCount"] == null)
+            {
+                Response.Write("<script>alert('Session has expired. Please refresh the page to start a new game.')</script>");
+                return;
+            }
+            int userNumber = Convert.ToInt32(txtNumberGuess.Text);
+            int randomNumber = (int)Session["RandomNumber"];
+            int attemptCount = (int)Session["AttemptCount"];
+            attemptCount--;
+            Session["AttemptCount"] = attemptCount;
+            if (userNumber > 100 )
+            {
+                lblMessage.Text = "please Enter number between 1 to 100";
+            }
+            else
+            {
+                lblMessage.Text = " ";
+                if (attemptCount <= 0)
+                {
+                    Response.Write("<script>alert('You lost the game')</script>");
+                    Session["RandomNumber"] = null;
+                    lblattempts.Text = "You have " + attemptCount + " attempts left.";
+                    Response.Redirect("Lost.aspx");
+                }
+                else
+                {
+                    if (randomNumber == userNumber)
+                    {
+
+                        Response.Write("<script>alert('" + randomNumber + " is correct.')</script>");
+                        Response.Write("<script>alert('congratulations Won in " + (10 - attemptCount) + " Attempt.')</script>");
+                        Session["RandomNumber"] = null;
+                        Response.Redirect("Won.aspx");
+                        return;
+                    }
+                    else if (randomNumber < userNumber)
+                    {
+                        Response.Write("<script>alert('" + userNumber + " is too high')</script>");
+                        lblattempts.Text = "You have " + attemptCount + " attempts left.";
+                    }
+                    else
+                    {
+                        Response.Write("<script>alert('" + userNumber + " is too low')</script>");
+                        lblattempts.Text = "You have " + attemptCount + " attempts left.";
+                    }
+                }
+                txtNumberGuess.Text = "";
+            }
+        }
+    }
+}
